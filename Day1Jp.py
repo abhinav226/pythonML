4311721c40af0eb27b2bfee5bed7921afbde3014
abhinav226 <76432426+abhinav226@users.noreply.github.com>
2022-04-02 19:02:49 +0530

    Add files via upload

---
+++ Day1Jp.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+####it should be writtben as print not print
+
+
+# # data Types
+
+# In[8]:
+
+
+x=12
+print(x)
+
+
+# In[9]:
+
+
+typeof(x)
+
+
+# In[10]:
+
+
+type(x)
+
+
+# In[11]:
+
+
+x=12.2
+
+
+# In[12]:
+
+
+type(x)
+
+
+# In[13]:
+
+
+course="asdad"
+type(course)
+
+
+# In[14]:
+
+
+course='asdad'
+type(course)
+
+
+# In[15]:
+
+
+z=false
+type(z)
+
+
+# In[16]:
+
+
+course=False
+type(course)
+
+
+# In[17]:
+
+
+names="abhinav","rudresh"
+
+
+# In[18]:
+
+
+names
+
+
+# In[ ]:
+
+
+
+
